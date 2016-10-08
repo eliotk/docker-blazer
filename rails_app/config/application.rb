6c1231acc92b768b8034e052bfdee5ef8689d6f2
Eliot Kristan <eliot.k@gmail.com>
2016-10-08 09:52:06 -0400

    Configure the rails app to log to stdout
    Since we're running the app in the docker container context we always
    want to log to stdout so the logs surface in the docker logs

--- rails_app/config/application.rb
+++ rails_app/config/application.rb
@@ -22,5 +22,8 @@ module BlazerApp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # always log out to stdout since we're running this app in a docker container
+    config.logger = Logger.new(STDOUT)
   end
 end
